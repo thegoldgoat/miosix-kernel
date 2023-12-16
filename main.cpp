1e98ac206c031cfc501edc89fe2613a17329f035
Simone <s.paternoster@libero.it>
2023-12-16 15:39:34 +0100

    Added folder multithreaded tests: create, delete, rename

--- main.cpp
+++ main.cpp
@@ -11,6 +11,50 @@
 using namespace std;
 using namespace miosix;
 
+void createDir(int dirNumber) {
+  char dirname[25];
+  snprintf(dirname, sizeof(dirname), "/sd/directory%d", dirNumber);
+  iprintf("Trying to create %s\n", dirname);
+
+  mkdir(dirname, 0755);
+
+  struct stat sb;
+  if(stat(dirname, &sb) == 0)
+    iprintf("The folder %s exists\n", dirname);
+  else
+    iprintf("The folder %s does not exist (INCORRECT BEHAVIOUR)\n", dirname);
+}
+
+void deleteDir(int dirNumber) {
+  char dirname[25];
+  snprintf(dirname, sizeof(dirname), "/sd/directory%d", dirNumber);
+  iprintf("Trying to delete %s\n", dirname);
+
+  rmdir(dirname);
+
+  struct stat sb;
+  if(stat(dirname, &sb) == 0)
+    iprintf("The folder %s exists (INCORRECT BEHAVIOUR)\n", dirname);
+  else
+    iprintf("The folder %s does not exist\n", dirname);
+}
+
+void renameDir(int dirNumber, int newDirNumber) {
+  char dirname[25];
+  snprintf(dirname, sizeof(dirname), "/sd/directory%d", dirNumber);
+
+  char newdirname[25];
+  snprintf(newdirname, sizeof(newdirname), "/sd/directory%d", newDirNumber);
+
+  rename(dirname, newdirname);
+
+  struct stat sb;
+  if(stat(dirname, &sb) != 0 && stat(dirname, &sb), &sb)
+    iprintf("Rename went right. %d -> %d\n", dirNumber, newDirNumber);
+  else
+    iprintf("Rename went wrong.\n");
+}
+
 void tryReadFile(int fileNumber) {
   char filename[20];
   snprintf(filename, sizeof(filename), "/sd/test%d.txt", fileNumber);
@@ -125,6 +169,50 @@ int main() {
   }
 
   iprintf("--- End mixed read/write test on multithreading ---\n");
+
+  iprintf("--- Begin folder creation test on multithreading ---\n");
+  threads.clear();
+
+  for (int i = 0; i < 10; i++) {
+    threads.emplace_back(thread(createDir, i));
+  }
+
+  for (auto &t : threads) {
+    t.join();
+  }
+
+  iprintf("--- End folder creation test on multithreading ---\n");
+
+  iprintf("--- Begin folder deletion test on multithreading ---\n");
+  threads.clear();
+
+  for (int i = 0; i < 10; i++) {
+    threads.emplace_back(thread(deleteDir, i));
+  }
+
+  for (auto &t : threads) {
+    t.join();
+  }
+
+  iprintf("--- End folder deletion test on multithreading ---\n");
+
+  iprintf("--- Begin folder renaming test on multithreading ---\n");
+
+  for (int i = 0; i < 10; i++) {
+    createDir(i);
+  }
+
+  threads.clear();
+
+  for (int i = 0; i < 10; i++) {
+    threads.emplace_back(thread(renameDir, i, i + 10));
+  }
+
+  for (auto &t : threads) {
+    t.join();
+  }
+
+  iprintf("--- End folder renaming test on multithreading ---\n");
 
   iprintf("Main end\n");
 }
